ef8eddf401fbd7faba8a645c4f09421a3f2a26d6
Prashishgt <prashish24@gmail.com>
2023-09-01 20:11:33 +0545

    Added image upload

--- admin/components/ui/image-upload.tsx
+++ admin/components/ui/image-upload.tsx
@@ -1,13 +1,81 @@
 "use client";
 
-import React from "react";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { ImagePlus, Trash } from "lucide-react";
+import Image from "next/image";
+
+import { CldUploadWidget } from "next-cloudinary";
 
 interface ImageUploadPropsI {
   disabled?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
 }
 
-const ImageUpload = (props: Props) => {
-  return <div>ImageUpload</div>;
+const ImageUpload = ({
+  onChange,
+  onRemove,
+  disabled,
+  value,
+}: ImageUploadPropsI) => {
+  const [isMounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const onUpload = (result: any) => {
+    onChange(result.info.secure_url);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+  return (
+    <div>
+      <div className="mb-4 flex items-center gap-4">
+        {value.map((url) => (
+          <div
+            key={url}
+            className="relative w-[120px] h-[120px] rounded=md overflow-hidden"
+          >
+            <div className="z-10 absolute top-2 right-2">
+              <Button
+                type="button"
+                onClick={() => onRemove(url)}
+                variant="destructive"
+                size="icon"
+              >
+                <Trash className="h-4 w-4" />
+              </Button>
+            </div>
+            <Image fill className="object-cover" alt="Image" src={url} />
+          </div>
+        ))}
+      </div>
+      <CldUploadWidget onUpload={onUpload} uploadPreset="cyp9xqqi">
+        {({ open }) => {
+          const onClick = () => {
+            open();
+          };
+          return (
+            <Button
+              type="button"
+              disabled={disabled}
+              variant="secondary"
+              onClick={onClick}
+              className="hover:bg-slate-300"
+            >
+              <ImagePlus className="h-4 w-4 mr-2" />
+              Upload an Image
+            </Button>
+          );
+        }}
+      </CldUploadWidget>
+    </div>
+  );
 };
 
 export default ImageUpload;
